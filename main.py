b4415361a0af0fd3469d9221dd7474a5ff7b79c0
kaneda <samflynn19121@gmail.com>
2020-08-24 09:32:17 +0200

    organize stuff

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
+
+from models import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -16,26 +17,6 @@ testset = torchvision.datasets.MNIST(root="./data", train=False,
 										download=True, transform=transforms.ToTensor())
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
 											shuffle=True, num_workers=2)
-
-test_train = False
-
-class Net(nn.Module):
-	def __init__(self):
-		super(Net, self).__init__()
-		self.fc1 = nn.Linear(784, 800)
-		self.relu = nn.ReLU()
-		self.fc2 = nn.Linear(800, 1600)
-		self.fc3 = nn.Linear(1600, 10)
-
-
-	def forward(self, x):
-		x = self.fc1(x)
-		x = self.relu(x)
-		x = self.fc2(x)
-		x = self.relu(x)
-		x = self.fc3(x)
-
-		return x
 
 net = Net().to(device)
 
@@ -82,9 +63,11 @@ def test():
 
 		print('Accuracy: {} %'.format(100 * correct / total))
 
+
+
 test_train = True
 plot_loss = True
-save_model = True
+save_model = False
 
 if test_train is True:
 	train()
@@ -95,4 +78,5 @@ if plot_loss is True:
 	plt.show()
 
 if save_model is True:
-    torch.save(net.state_dict(), 'MNIST_model.pt')
+	 torch.save(net.state_dict(), 'MNIST_model.pt')
+
---
+++ models/fully_connected.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.fc1 = nn.Linear(784, 800)
+		self.relu = nn.ReLU()
+		self.fc2 = nn.Linear(800, 1600)
+		self.fc3 = nn.Linear(1600, 10)
+
+
+	def forward(self, x):
+		x = self.fc1(x)
+		x = self.relu(x)
+		x = self.fc2(x)
+		x = self.relu(x)
+		x = self.fc3(x)
+
+		return x
